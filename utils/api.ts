800686243ad73c3b9da8c782acb17ff24511bb75
aleksey-ruban <65021347+aleksey-ruban@users.noreply.github.com>
2025-03-18 23:41:10 +0300

    Api for slices

---
+++ utils/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://10.0.1.69",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    timeout: 4000,
+});
+
+export default api;
